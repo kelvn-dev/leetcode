8fb6de34e76a2cd9f7d1182202e262a74d3ff906
Vũ Gia Khánh <khanhvg@vinbrain.net>
2024-10-22 10:44:35 +0700

    add top k element pattern

---
+++ pattern/TopKElement.java
@@ -0,0 +1,74 @@
+package pattern;
+
+import java.util.*;
+
+/**
+ * Use this pattern to find the top k largest or smallest elements in an unsorted array of data using heaps
+ * Use sorting will be simpler but time complexity will be higher
+ *
+ * Sample Problem:
+ * Find the k-th largest element in an unsorted array.
+ *
+ * Example:
+ * Input: nums = [3, 2, 1, 5, 6, 4], k = 2
+ * Output: 5
+ *
+ * Explanation:
+ * Use a min-heap of size k to keep track of the k largest elements -> peek will be the top K largest.
+ * Iterate through the array, adding elements to the heap.
+ * If the heap size exceeds k, remove the smallest element from the heap.
+ * The root of the heap will be the k-th largest element.
+ */
+public class TopKElement {
+  public static void main(String[] args) {
+    int[] array = new int[]{3, 2, 1, 5, 6, 4};
+    int k = 2;
+    int result = getTopKElementUsingHeap(array, k);
+    System.out.println(result);
+  }
+
+  public static int getTopKElementUsingHeap(int[] array, int k) {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<>(k);
+    for(int i: array) {
+      minHeap.add(i);
+      if (minHeap.size() > k) {
+        minHeap.poll();
+      }
+    }
+    return minHeap.peek();
+  }
+
+  /**
+   * Interview question
+   */
+  public static int getSecondMaxIndexWith1Loop(int[] array) {
+    if (array.length == 0) {
+      return -1;
+    }
+    if (array.length == 1) {
+      return array[0];
+    }
+    int maxIndex = 0;
+    int secondMaxIndex = 0;
+    for(int i = 0; i < array.length; i++) {
+      if (array[i] > array[maxIndex]) {
+        secondMaxIndex = maxIndex;
+        maxIndex = i;
+      } else if (array[i] > array[secondMaxIndex]) {
+        secondMaxIndex = i;
+      }
+    }
+    return secondMaxIndex;
+  }
+
+  public static int getIndexOfTopKElement(int[] array, int k) {
+    PriorityQueue<Integer[]> minHeap = new PriorityQueue<>(k, Comparator.comparingInt(o -> o[0]));
+    for(int i = 0; i < array.length; i++) {
+      minHeap.add(new Integer[]{array[i], i});
+      if (minHeap.size() > k) {
+        minHeap.poll();
+      }
+    }
+    return Objects.isNull(minHeap) ? -1:  minHeap.peek()[1];
+  }
+}
