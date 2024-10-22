8b14c5615bfe10cd57cac17f98be55015fc3d693
Vũ Gia Khánh <khanhvg@vinbrain.net>
2024-10-22 10:01:19 +0700

    refactor and remove Test class

--- pattern/InPlaceReversal.java
+++ pattern/InPlaceReversal.java
@@ -78,4 +78,16 @@ public class InPlaceReversal {
       temp = temp.getNext();
     }
   }
+
+  public static void main(String[] args) {
+    InPlaceReversal inPlaceReversal = new InPlaceReversal();
+    inPlaceReversal.addFirst(5);
+    inPlaceReversal.addFirst(4);
+    inPlaceReversal.addFirst(3);
+    inPlaceReversal.addFirst(2);
+    inPlaceReversal.addFirst(1);
+//    inPlaceReversal.reverse();
+    inPlaceReversal.reverseBetween(2, 4);
+    inPlaceReversal.print();
+  }
 }
